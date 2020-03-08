5d2d241f6ab73b2cb6ae10b3fbbdbb8fb40129f7
Roni Ardiyanto <rcain2@avc.edu>
2020-03-08 20:43:14 +0700

    use session

--- controllers/root.js
+++ controllers/root.js
@@ -6,7 +6,7 @@ const { getAll } = require('../models/note');
 //when user acces homepage
 async function index(req, res) {
   const notes =  await getAll();
-  res.cookie('master', 'Yum!');
+  req.session.username = 'Annonymous';
   res.render('index', { notes: notes });
 }
 
